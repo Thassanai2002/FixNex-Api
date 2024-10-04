2e75e534b5030716d086704fb1f10dc797e4894c
Thassanai2002 <thassanai.tangprom@gmail.com>
2024-10-04 20:07:01 +0700

    add.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -31,6 +31,23 @@ import { User } from './Entity/userEntity';
 import { Product } from './Entity/productEntity';
 import { UserService } from './Repository/userRepository';
 import { UserController } from './Controller/userController';
+import { ProductController } from './Controller/productController';
+import { ProductService } from './Repository/productRepository';
+import { Order } from './Entity/orderEntity';
+import { OrderController } from './Controller/orderController';
+import { OrderService } from './Repository/orderRepository';
+import { OrderItem } from './Entity/orderItemEntity';
+import { OrderItemController } from './Controller/orderItemController';
+import { OrderItemService } from './Repository/orderItemRepository';
+import { Trainer } from './Entity/trainerEntity';
+import { TrainerController } from './Controller/trainerController';
+import { TrainerService } from './Repository/trainerRepository';
+import { TrainerRantals } from './Entity/trainerRantalsEntity';
+import { TrainerRantalsService } from './Repository/trainerRantalsRepository';
+import { TrainerRantalsController } from './Controller/trainerRantalsController';
+import { UserSpendingController } from './Controller/userSpendingController';
+import { UserSpendingService } from './Repository/userSpendingRepository';
+import { UserSpending } from './Entity/userSpendingEntity';
 
 @Module({
   imports: [
@@ -41,19 +58,44 @@ import { UserController } from './Controller/userController';
       username: 'jackky',
       password: 'jackky',
       database: 'fitnex',
-      entities: [User, Product],
+      entities: [
+        User,
+        Product,
+        Order,
+        OrderItem,
+        Trainer,
+        TrainerRantals,
+        UserSpending
+      ],
       synchronize: true,
     }),
-    TypeOrmModule.forFeature([User]),  // นำ Entity User มาใช้ใน TypeORM
+    TypeOrmModule.forFeature([
+      User,
+      Product,
+      Order,
+      OrderItem,
+      Trainer,
+      TrainerRantals,
+      UserSpending
+    ]),
   ],
-  controllers: [UserController],  // เพิ่ม UserController
-  providers: [UserService],       // เพิ่ม UserService
+  controllers: [
+    UserController,
+    ProductController,
+    OrderController,
+    OrderItemController,
+    TrainerController,
+    TrainerRantalsController,
+    UserSpendingController
+  ],
+  providers: [
+    UserService,
+    ProductService,
+    OrderService,
+    OrderItemService,
+    TrainerService,
+    TrainerRantalsService,
+    UserSpendingService
+  ],
 })
 export class AppModule {}
-
-
-// host: '10.0.0.94',
-// port: 3306,
-// username: 'jackky',
-// password: 'jackky',
-// database: 'fitnex',
