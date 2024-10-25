c90b369e11dd3dae864a2c15e341dd63d6b509ae
Thassanai2002 <thassanai.tangprom@gmail.com>
2024-10-26 05:29:20 +0700

    checkUserExists

--- src/Controller/userController.ts
+++ src/Controller/userController.ts
@@ -1,29 +1,46 @@
-import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Patch,
+  ConflictException,
+} from '@nestjs/common';
 import { User } from 'src/Entity/userEntity';
 import { UserService } from 'src/Repository/userRepository';
 import { serviceAll } from 'src/Service/Service';
 
 @Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService, private readonly serviceAll: serviceAll) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly serviceAll: serviceAll,
+  ) {}
 
   @Get()
   findAll(): Promise<User[]> {
     return this.userService.findAll();
   }
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string): Promise<User> {
-  //   return this.userService.findOne(+id);
-  // }
+  @Get(':id')
+  async findTrainerUserProgamsEnrollments(
+    @Param('id') id: number,
+  ): Promise<any> {
+    return await this.serviceAll.findTrainerUserProgamsEnrollments(+id);
+  }
 
-@Get(':id')
-async findTrainerUserProgamsEnrollments(@Param('id') id: number): Promise<any> {
-  return await this.serviceAll.findTrainerUserProgamsEnrollments(+id);
-}
-  
   @Post()
-  create(@Body() user: User): Promise<User> {
+  async create(@Body() user: User): Promise<User> {
+    const userExists = await this.userService.checkUserExists(
+      user.user_name,
+      user.phone,
+      user.email,
+    );
+    if (userExists) {
+      throw new ConflictException('User name, phone, or email already exists!'); // ยกเว้นข้อผิดพลาด
+    }
     return this.userService.create(user);
   }
 
@@ -38,7 +55,10 @@ async findTrainerUserProgamsEnrollments(@Param('id') id: number): Promise<any> {
   }
 
   @Patch(':id')
-  update(@Param('id') user_id: string, @Body() user: Partial<User>): Promise<User> {
+  update(
+    @Param('id') user_id: string,
+    @Body() user: Partial<User>,
+  ): Promise<User> {
     return this.userService.update(+user_id, user);
   }
 }
